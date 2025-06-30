980e1a78b800bff8a9adffbdba14eb752655aae4
bell s <kimseunghwan7777@gmail.com>
2025-06-30 07:23:49 +0000

    fix: apply stash change

---
+++ payment/src/main/java/untitled/domain/PointChargeRequested.java
@@ -0,0 +1,17 @@
+package untitled.domain;
+
+import java.util.*;
+import lombok.*;
+import untitled.domain.*;
+import untitled.infra.AbstractEvent;
+
+@Data
+@ToString
+public class PointPaymentRequested extends AbstractEvent {
+
+    private Long id;
+    private Long readerId;
+    private Integer point;
+    private String impUid;
+    private Integer cost; // 결제 금액
+}
