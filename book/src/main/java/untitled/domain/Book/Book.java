cca898f16752b569881736ae1bc7432425d97ad2
doyooon kwon <ian661087@gmail.com>
2025-07-01 04:54:56 +0000

    feat : 같은 원고에 대한 중복 출판 방지 기능 추가

--- book/src/main/java/untitled/domain/Book/Book.java
+++ book/src/main/java/untitled/domain/Book/Book.java
@@ -32,5 +32,7 @@ public class Book {
 
     private Long views;
 
+    private Long manuscriptId;
+
 }
 //>>> DDD / Aggregate Root
--- book/src/main/java/untitled/domain/Book/BookRegistered.java
+++ book/src/main/java/untitled/domain/Book/BookRegistered.java
@@ -1,9 +1,6 @@
 package untitled.domain.Book;
 
-import java.time.LocalDate;
-import java.util.*;
 import lombok.*;
-import untitled.domain.*;
 import untitled.infra.AbstractEvent;
 
 //<<< DDD / Domain Event
