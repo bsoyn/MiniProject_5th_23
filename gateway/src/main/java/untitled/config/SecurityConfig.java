2a08bc28c231864a92a3e1f1ff3c146e68d015db
SoYeon Bae <soye0200@gmail.com>
2025-07-01 00:56:53 +0000

    현재 상황: 회원가입, 로그인 성공하지만 인증 실패

--- gateway/src/main/java/untitled/config/SecurityConfig.java
+++ gateway/src/main/java/untitled/config/SecurityConfig.java
@@ -13,11 +13,9 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
-            .csrf().disable()      // ★ 1. CSRF 보호 기능 비활성화
-            .formLogin().disable() // ★ 2. 폼 기반 로그인 비활성화
-            .httpBasic().disable() // ★ 3. HTTP Basic 인증 비활성화
-            .authorizeExchange()   // ★ 4. 경로별 권한 설정 시작
-                .anyExchange().permitAll() // ★ 5. 모든 경로를 일단 허용
+            .csrf().disable()      // CSRF 보호 기능 비활성화
+            .authorizeExchange()
+                .anyExchange().permitAll() // 모든 경로 허용
                 .and()
             .build();
     }
