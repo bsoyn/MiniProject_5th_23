ad28ce488e7722a56a08faec7054b4adda942ba9
SoYeon Bae <soye0200@gmail.com>
2025-07-03 06:08:29 +0000

    application.yml 설정 추가, client들의 url  수정

--- gateway/src/main/java/untitled/client/AdminServiceClient.java
+++ gateway/src/main/java/untitled/client/AdminServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import untitled.dto.ValidationRequest;
 import untitled.dto.ValidationResponse;
 
-@FeignClient(name = "admin", url = "http://localhost:8085")
+@FeignClient(name = "admin", url = "${client.urls.admin}")
 public interface AdminServiceClient {
     @PostMapping(value = "/internal/validate", consumes = "application/json")
     ValidationResponse validate(ValidationRequest request);
